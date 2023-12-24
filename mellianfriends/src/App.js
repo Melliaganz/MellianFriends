fae4196ae6418f73b27f4e37230e593abe28ea4b
Lucas Lengrand <lucaslengranddev@gmail.com>
2023-12-24 01:29:21 +0100

    création écran login

--- mellianfriends/src/App.js
+++ mellianfriends/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Login/Register';
+import Accueil from './Pages/Accueil/Accueil';
+import Header from './components/Header'; 
+import Loader from './components/Loader';
+import { auth } from './firebase/firebase';
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setUser(user);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const handleLogout = () => {
+    auth.signOut()
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {
+        console.error('Error during logout:', error.message);
+      });
+  };
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Header user={user} onDarkModeToggle={toggleDarkMode} darkMode={darkMode} />
+      {loading && <Loader />}
+
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Navigate to="/accueil" replace />} />
+        ) : (
+          <Route path="/" element={<Login />} />
+        )}
+
+        {user ? (
+          <Route path="/register" element={<Navigate to="/accueil" replace />} />
+        ) : (
+          <Route path="/register" element={<Register />} />
+        )}
+
+        {user ? (
+          <Route path="/accueil" element={<Accueil />} />
+        ) : (
+          <Route path="*" element={<Navigate to="/" replace />} />
+        )}
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
